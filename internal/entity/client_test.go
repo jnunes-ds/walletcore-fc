410f5c84eefa1ad4054992df40e80a4546fdc19c
Denison Junior <denisonprobable13@gmail.com>
2025-07-07 12:18:41 -0300

    test: create update method for client entity

--- internal/entity/client_test.go
+++ internal/entity/client_test.go
@@ -6,10 +6,10 @@ import (
 )
 
 func TestCreateNewClient(t *testing.T) {
-	client, err := NewClient("John Doe", "jhon@email.com")
+	client, err := NewClient("Jhon Doe", "jhon@email.com")
 	assert.Nil(t, err)
 	assert.NotNil(t, client)
-	assert.Equal(t, "John Doe", client.Name)
+	assert.Equal(t, "Jhon Doe", client.Name)
 	assert.Equal(t, "jhon@email.com", client.Email)
 }
 
@@ -17,4 +17,18 @@ func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
 	client, err := NewClient("", "")
 	assert.NotNil(t, err)
 	assert.Nil(t, client)
+}
+
+func TestUpdateClient(t *testing.T) {
+	client, _ := NewClient("Jhon Doe", "jhon@email.com")
+	err := client.Update("Jane Doe Updated", "updated_jhon@email.com")
+	assert.Nil(t, err)
+	assert.Equal(t, "Jane Doe Updated", client.Name)
+	assert.Equal(t, "updated_jhon@email.com", client.Email)
+}
+
+func TestUpdateClientWithInvalidArgs(t *testing.T) {
+	client, _ := NewClient("Jhon Doe", "jhon@email.com")
+	err := client.Update("", "updated_jhon@email.com")
+	assert.Error(t, err)
 }
