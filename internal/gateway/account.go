610856d441b6222d089b3523c890a4a713574292
Denison Junior <denisonprobable13@gmail.com>
2025-07-07 17:56:42 -0300

    feat: create account usecase

---
+++ internal/gateway/account.go
@@ -0,0 +1,8 @@
+package gateway
+
+import "github.com/jnunes-ds/walletcore-fc/internal/entity"
+
+type AccountGateway interface {
+	Save(account *entity.Account) error
+	FindById(id string) (*entity.Account, error)
+}
---
+++ internal/usecase/create_account/create_account.go
@@ -0,0 +1,41 @@
+package createaccount
+
+import (
+	"github.com/jnunes-ds/walletcore-fc/internal/entity"
+	"github.com/jnunes-ds/walletcore-fc/internal/gateway"
+)
+
+type CreateAccountInputDTO struct {
+	ClientId string
+}
+
+type CreateAccountOutputDTO struct {
+	ID string
+}
+
+type CreateAccountUseCase struct {
+	AccountGateway gateway.AccountGateway
+	ClientGateWay  gateway.ClientGateway
+}
+
+func NewCreateAccountUseCase(a gateway.AccountGateway, c gateway.ClientGateway) *CreateAccountUseCase {
+	return &CreateAccountUseCase{
+		AccountGateway: a,
+		ClientGateWay:  c,
+	}
+}
+
+func (uc *CreateAccountUseCase) Execute(input CreateAccountInputDTO) (*CreateAccountOutputDTO, error) {
+	client, err := uc.ClientGateWay.Get(input.ClientId)
+	if err != nil {
+		return nil, err
+	}
+	account := entity.NewAccount(client)
+	err = uc.AccountGateway.Save(account)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateAccountOutputDTO{
+		ID: account.ID,
+	}, nil
+}
