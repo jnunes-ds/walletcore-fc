e8fa5fa8a3db51126dae89f6ea93ee84c6562157
Denison Junior <denisonprobable13@gmail.com>
2025-07-07 11:58:26 -0300

    feat: create client entity

--- internal/entity/client.go
+++ internal/entity/client.go
@@ -1,6 +1,10 @@
 package entity
 
-import "time"
+import (
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
 
 type Client struct {
 	ID        string
@@ -8,4 +12,32 @@ type Client struct {
 	Email     string
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func NewClient(name string, email string) (*Client, error) {
+	client := &Client{
+		ID:        uuid.New().String(),
+		Name:      name,
+		Email:     email,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	err := client.Validade()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func (c *Client) Validade() error {
+	if c.Name == "" {
+		return errors.New("name is required")
+	}
+	if c.Email == "" {
+		return errors.New("email is required")
+	}
+	return nil
 }
