0a80f55932a91802227d58943c55c509cc579d6c
Denison Junior <denisonprobable13@gmail.com>
2025-08-13 14:44:21 -0300

    feat: execute migrations when server is running

--- ecommerce/src/main.ts
+++ ecommerce/src/main.ts
@@ -1,8 +1,30 @@
+/*
+	eslint-disable @typescript-eslint/no-unsafe-assignment,
+	@typescript-eslint/no-unsafe-call,
+	@typescript-eslint/no-unsafe-member-access
+*/
+import { execSync } from 'node:child_process';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+function runPrismaMigrations() {
+	console.log('Checking and applying Prisma migrations...');
+	try {
+		execSync('npx prisma migrate deploy', { stdio: 'inherit' });
+		console.log('Prisma migrations applied successfully.');
+	} catch (error) {
+		console.error('Failed to apply Prisma migrations:', error);
+		process.exit(1);
+	}
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	runPrismaMigrations();
+	const app = await NestFactory.create(AppModule);
+	const configService = app.get(ConfigService);
+	const port = configService.get<number>('PORT') || 3000;
+	await app.listen(port);
+	console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
