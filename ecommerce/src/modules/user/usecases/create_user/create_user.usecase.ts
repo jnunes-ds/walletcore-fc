72c3a542a7ea2600f034c2b2b29601758e756025
Denison Junior <denisonprobable13@gmail.com>
2025-08-20 14:05:50 -0300

    test: create usecase to create a new user

--- ecommerce/src/modules/user/usecases/create_user/create_user.usecase.ts
+++ ecommerce/src/modules/user/usecases/create_user/create_user.usecase.ts
@@ -33,8 +33,7 @@ export class CreateUserUsecase
 				email: userCreated.email,
 				isSeller: userCreated.isSeller,
 			};
-		} catch (error) {
-			console.error(error);
+		} catch {
 			throw new Error('Error creating user');
 		}
 	}
