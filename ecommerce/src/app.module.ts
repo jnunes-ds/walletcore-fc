3217e79b946b4908a80ac695db6ed89e05bd9173
Denison Junior <denisonprobable13@gmail.com>
2025-08-28 16:26:17 -0300

    fix: use right way to get prisma service

--- ecommerce/src/app.module.ts
+++ ecommerce/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/api/user.module';
+import { ProductModule } from './modules/product/api/product.module';
 
 @Module({
 	imports: [
 		// Configura o ConfigModule para ser global, lendo o arquivo .env
 		ConfigModule.forRoot({ isGlobal: true }),
 		UserModule,
+		ProductModule,
 	],
 	controllers: [],
 	providers: [],
--- ecommerce/src/modules/user/api/user.service.ts
+++ ecommerce/src/modules/user/api/user.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUserUsecase } from '@modules/user/usecases/create_user/create_user.usecase';
 import { PrismaService } from '@database/prisma.service';
 
 @Injectable()
 export class UserService {
+	constructor(private readonly databaseService: PrismaService) {}
 	async create(createUserDto: CreateUserDto) {
-		const databaseService = new PrismaService();
-		const usecase = new CreateUserUsecase(databaseService);
+		const usecase = new CreateUserUsecase(this.databaseService);
+
 		await usecase.execute({
 			name: createUserDto.name,
 			email: createUserDto.email,
